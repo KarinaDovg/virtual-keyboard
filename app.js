beed71384d92294de796530dcb38b00e5eedd4f6
KarinaDovg <korzhenevskaya.2020@mail.ru>
2023-04-30 16:56:29 +0300

    feat: connect and setting package in project

---
+++ app.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import keyboardRussian from './assets/russian.js';
+import keyboardEnglish from './assets/english.js';
+
+document.body.innerHTML = keyboardRussian;
---
+++ assets/russian.js
@@ -0,0 +1,5 @@
+export default `
+
+<header>12321</header>
+<script type="module" src="app.js"></script>
+`;
