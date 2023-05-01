b628f187fd60d3195e8bef67d6cc86c092016c7a
KarinaDovg <korzhenevskaya.2020@mail.ru>
2023-05-01 15:41:40 +0300

    feat: add switch language with CapsLock

--- app.js
+++ app.js
@@ -2,5 +2,31 @@
 import keyboardRussian from './assets/russian.js';
 import keyboardEnglish from './assets/english.js';
 
-document.body.innerHTML = keyboardEnglish;
-document.body.innerHTML = keyboardRussian;
+function initialization() {
+  if (localStorage.getItem('Language') === null) {
+    localStorage.setItem('Language', 'en');
+    document.body.innerHTML = keyboardEnglish;
+  } else if (localStorage.getItem('Language') === 'en') {
+    document.body.innerHTML = keyboardEnglish;
+  } else {
+    document.body.innerHTML = keyboardRussian;
+  }
+}
+
+function changeLanguage() {
+  if (localStorage.getItem('Language') === 'en') {
+    document.body.innerHTML = keyboardRussian;
+    localStorage.setItem('Language', 'ru');
+  } else {
+    document.body.innerHTML = keyboardEnglish;
+    localStorage.setItem('Language', 'en');
+  }
+}
+
+initialization();
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'CapsLock') {
+    changeLanguage();
+  }
+});
